f506678c860acadbd90f07f1d0588493ccc7eb24
Cristian <cristian.beltran.paco@gmail.com>
2021-09-20 12:15:06 -0400

    Consultas y creacion de archivo index.html

--- project/db.py
+++ project/db.py
@@ -1,12 +1,5 @@
 import cx_Oracle
-ip = '192.168.2.250' #ip
-port = 1521  #port
-SID = 'ATBPRU'
-dsn_tns = cx_Oracle.makedsn(ip, port, SID)
-connect = cx_Oracle.connect('FA53', 'FA53', dsn_tns)
 
-cursor = connect.cursor()
-cursor.execute('SELECT * FROM NAF')
-registros = cursor.fetchall()
-for r in registros:
-    print(str(r))
+connection = cx_Oracle.connect("FA53/FA53@192.168.2.250:1521/ATBPRU")
+print("Connection ..... OK")
+cursor = connection.cursor()
--- project/main.py
+++ project/main.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask,render_template
+import cx_Oracle
 
 app = Flask(__name__)
+connection = cx_Oracle.connect("FA53/FA53@192.168.2.250:1521/ATBPRU")
+cursor = connection.cursor()
+
+     
 
 @app.route('/')
-def raiz():
-    return "hola mundo"
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+def index():
+    sql = """
+        select NO_DOCU,GRUPO,NO_CLIENTE,FECHA,TOTAL,COD_CONTROL,CENTROD,TIPO_DOC,RUTA,NO_ORDEN
+        FROM interfaz_fe
+        where no_cia='01'  
+        and origen='TV'
+        and impreso='S'
+        and to_char(fecha,'yyyy')=to_char('2019')
+        and no_docu||no_orden||TIPO_DOC in (select distinct(no_factu||no_f300||tipo_doc)
+        from ARFAFL_DET
+        where no_cia='01'
+        and periodo = '2019')
+        order by fecha,no_docu;        j
+    """
+    result = cursor.execute(sql)
+    contex = {
+        'DATA':result
+    }
+    return render_template('index.html',contex=contex) 
